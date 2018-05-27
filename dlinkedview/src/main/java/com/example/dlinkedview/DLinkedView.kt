4caa0939462f26977ba2d83d3a5cac6578f09357
Anwesh43 <anweshthecool0@gmail.com>
2018-05-27 12:02:02 +0530

    adding DLinkedView

--- dlinkedview/src/main/java/com/example/dlinkedview/DLinkedView.kt
+++ dlinkedview/src/main/java/com/example/dlinkedview/DLinkedView.kt
@@ -3,3 +3,26 @@ package com.example.dlinkedview
 /**
  * Created by anweshmishra on 27/05/18.
  */
+
+import android.content.Context
+import android.view.*
+import android.graphics.*
+
+class DLinkedView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+}
