43d950da2257b004e5beb4518295020ef427a5fe
Anwesh43 <anweshthecool0@gmail.com>
2018-05-27 12:53:57 +0530

    adding DNode class

--- dlinkedview/src/main/java/com/example/dlinkedview/DLinkedView.kt
+++ dlinkedview/src/main/java/com/example/dlinkedview/DLinkedView.kt
@@ -8,6 +8,8 @@ import android.content.Context
 import android.view.*
 import android.graphics.*
 
+val D_NODES = 5
+
 class DLinkedView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -75,6 +77,61 @@ class DLinkedView(ctx : Context) : View(ctx) {
             if (animated) {
                 animated = false
             }
+        }
+    }
+
+    data class DNode(var i : Int = 0, val state : State = State()) {
+
+        var prev : DNode? = null
+
+        var next : DNode? = null
+
+        init {
+            this.addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < D_NODES - 1) {
+                next = DNode(i +1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            val w : Float = canvas.width.toFloat()
+            val h : Float = canvas.height.toFloat()
+            val gap : Float = h / D_NODES
+            paint.strokeWidth = Math.min(w, h)/45
+            paint.strokeCap = Paint.Cap.ROUND
+            paint.color = Color.parseColor("#4CAF50")
+            canvas.save()
+            canvas.translate(w/2, gap * i)
+            canvas.save()
+            canvas.translate(0f, gap * state.scales[2])
+            canvas.drawLine(0f, 0f, 0f, gap, paint)
+            canvas.restore()
+            canvas.drawArc(RectF(-gap/2, 0f, gap/2, gap), -90f + 180f * state.scales[1], 180f * state.scales[0] * (1 - state.scales[1]), false, paint)
+            canvas.restore()
+        }
+
+        fun update(stopcb : (Float) -> Unit) {
+            state.update(stopcb)
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            state.startUpdating(startcb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DNode {
+            var curr : DNode? = this.prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
